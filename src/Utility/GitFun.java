6ecc25c08bbf5d01c16557615a745cae534a0bd5
Sheku114 <ab427902@dal.ca>
2020-08-05 21:56:23 -0300

    Added fun part of java

---
+++ src/Utility/GitFun.java
@@ -0,0 +1,13 @@
+package Utility;
+
+import java.util.Scanner;
+
+public class GitFun {
+    public static void main(String[] args) {
+
+        int i = 5;
+        System.out.println(i);
+    }
+
+
+}
