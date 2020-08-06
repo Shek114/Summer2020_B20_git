19d1eed7ff46934cb6a68d00c2f1143f24492a2a
Sheku114 <ab427902@dal.ca>
2020-08-05 21:29:00 -0300

    Added second git profile

---
+++ src/ReverseString.java
@@ -0,0 +1,8 @@
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String name = "Shekur";
+
+        System.out.println(name);
+    }
+}
--- src/Utility/ZombieAttack.java
+++ src/Utility/ZombieAttack.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class ZombieAttack {
     public static void main(String[] args) {
 
+        //
+        //
+
 
         Scanner scan=new Scanner(System.in);
         String str=scan.nextLine();
