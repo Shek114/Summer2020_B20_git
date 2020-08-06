d3074aa71153752bd871dbda6cb57157916a24ba
Sheku114 <ab427902@dal.ca>
2020-08-05 21:01:15 -0300

    First git profile

---
+++ src/Utility/ZombieAttack.java
@@ -0,0 +1,21 @@
+package Utility;
+
+import java.util.Scanner;
+
+public class ZombieAttack {
+    public static void main(String[] args) {
+
+
+        Scanner scan=new Scanner(System.in);
+        String str=scan.nextLine();
+        String word=scan.next();
+        int l=word.length()-1;
+        int count=0;
+        for (int i = 0; i <=str.length()-l ; i++) {
+            if(str.substring(i,i+l).equalsIgnoreCase(word)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
